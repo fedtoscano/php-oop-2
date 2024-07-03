c548ce656e47dd24df2b50e22cc12d1266e6eb6b
fedtoscano <federicotoscano611@gmail.com>
2024-07-04 01:14:26 +0200

    aggiunto css

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once __DIR__.'/db/db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Boolshop</title>
 </head>
 <body>
@@ -20,82 +21,85 @@ require_once __DIR__.'/db/db.php';
             <ul>
                 <?php foreach ($itemList as $key => $item) { ?>
                     <?php if($item instanceof Food) { ?>
-
-                        <div class="image-container">
-                            <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
-                        </div>
-
-                        <div class="info-container">
-                            <h3>
-                                <?= $item->getTitle(); ?>
-                            </h3>
-                            <p> 
-                                <?= $item->getCategory()?>
-                            </p>
-                            <p> 
-                                Prezzo: <?= $item->getPrice()?> euro
-                            </p>
-                            <p> 
-                                Peso Netto: <?= $item->getWeight()?>g
-                            </p>
-                            <p>
-                                Ingredienti: 
-                                <?php foreach ($item->getIngredients() as $ingredient) { ?>
-                                    <span><?= $ingredient ?>, </span>
-                                <?php } ?>
-                            </p>
-                        </div>
+                        <li>
+                            <div class="image-container">
+                                <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
+                            </div>
+    
+                            <div class="info-container">
+                                <h3>
+                                    <?= $item->getTitle(); ?>
+                                </h3>
+                                <p> 
+                                    <?= $item->getCategory()?>
+                                </p>
+                                <p> 
+                                    Prezzo: <?= $item->getPrice()?> euro
+                                </p>
+                                <p> 
+                                    Peso Netto: <?= $item->getWeight()?>g
+                                </p>
+                                <p>
+                                    Ingredienti: 
+                                    <?php foreach ($item->getIngredients() as $ingredient) { ?>
+                                        <span><?= $ingredient ?>, </span>
+                                    <?php } ?>
+                                </p>
+                            </div>
+                        </li>
 
                     <?php } ?>
 
                     <?php if($item instanceof Game) { ?>
-                    
-                        <div class="image-container">
-                            <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
-                        </div>
-
-                        <div class="info-container">
-                            <h3>
-                                <?= $item->getTitle(); ?>
-                            </h3>
-                            <p> 
-                                <?= $item->getCategory()?>
-                            </p>
-                            <p> 
-                                Prezzo: <?= $item->getPrice()?> euro
-                            </p>
-                            <p> 
-                                Caratteristiche: <?= $item->getDescription()?>
-                            </p>
-                            <p>
-                                Dimensioni: <?= $item->getSize() ?>
-                            </p>
-                        </div>
-                    
+                        <li>
+                                <div class="image-container">
+                                <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
+                                </div>
+        
+                                <div class="info-container">
+                                    <h3>
+                                        <?= $item->getTitle(); ?>
+                                    </h3>
+                                    <p> 
+                                        <?= $item->getCategory()?>
+                                    </p>
+                                    <p> 
+                                        Prezzo: <?= $item->getPrice()?> euro
+                                    </p>
+                                    <p> 
+                                        Caratteristiche: <?= $item->getDescription()?>
+                                    </p>
+                                    <p>
+                                        Dimensioni: <?= $item->getSize() ?>
+                                    </p>
+                                </div>
+                        </li>
                     <?php } ?>
 
                     <?php if($item instanceof Gadget) { ?>
-                        <div class="image-container">
-                            <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
-                        </div>
-
-                        <div class="info-container">
-                            <h3>
-                                <?= $item->getTitle(); ?>
-                            </h3>
-                            <p> 
-                                <?= $item->getCategory()?>
-                            </p>
-                            <p> 
-                                Prezzo: <?= $item->getPrice()?> euro
-                            </p>
-                            <p> 
-                                Materiale: <?= $item->getMaterial()?>
-                            </p>
-                            <p>
-                                Dimensioni: <?= $item->getSize() ?>
-                            </p>
-                        </div>
+                        <li>
+                            <div class="image-container">
+                                <img src="<?= $item->getImgUrl() ?>" alt="<?= $item->getTitle() ?>">                        
+                            </div>
+                        
+                            <div class="info-container">
+                                <h3>
+                                    <?= $item->getTitle(); ?>
+                                </h3>
+                                <p> 
+                                    <?= $item->getCategory()?>
+                                </p>
+                                <p> 
+                                    Prezzo: <?= $item->getPrice()?> euro
+                                </p>
+                                <p> 
+                                    Materiale: <?= $item->getMaterial()?>
+                                </p>
+                                <p>
+                                    Dimensioni: <?= $item->getSize() ?>
+                                </p>
+                            </div>
+                        </li>
                     <?php } ?>
 
                 <?php } ?>
